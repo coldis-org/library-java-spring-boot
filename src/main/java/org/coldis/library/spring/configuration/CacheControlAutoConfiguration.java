63a4f316e0ae663ffee5ac70fa0ccedad14af316
rvcoutinho <me@rvcoutinho.com>
2019-08-20 19:42:38 -0300

    - Enabling cache control;
    Signed-off-by: rvcoutinho <me@rvcoutinho.com>

---
+++ src/main/java/org/coldis/library/spring/configuration/CacheControlAutoConfiguration.java
@@ -0,0 +1,29 @@
+package org.coldis.library.spring.configuration;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Cache control auto configuration.
+ */
+@Configurable
+public class CacheControlAutoConfiguration implements WebMvcConfigurer {
+
+	/**
+	 * Resources not to cache.
+	 */
+	@Value(value = "#{'${spring.resources.cache.cachecontrol.ignore}'.split(',')}")
+	private String[] resourcesNotToCache;
+
+	/**
+	 * @see org.springframework.web.servlet.config.annotation.WebMvcConfigurer#addResourceHandlers(org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry)
+	 */
+	@Override
+	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+		registry.addResourceHandler(this.resourcesNotToCache).setCachePeriod(0).setCacheControl(CacheControl.noStore());
+
+	}
+}
