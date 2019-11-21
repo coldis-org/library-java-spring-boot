b39cc082dc3c50e3679ebf0651a006545b86ffa4
rvcoutinho <me@rvcoutinho.com>
2019-11-21 04:48:56 -0200

    - Fixing cache control;
    Signed-off-by: rvcoutinho <me@rvcoutinho.com>

--- src/main/java/org/coldis/library/spring/configuration/CacheControlAutoConfiguration.java
+++ src/main/java/org/coldis/library/spring/configuration/CacheControlAutoConfiguration.java
@@ -1,6 +1,8 @@
 package org.coldis.library.spring.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.web.ResourceProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -13,6 +15,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CacheControlAutoConfiguration implements WebMvcConfigurer {
 
 	/**
+	 * Resource properties.
+	 */
+	@Autowired
+	private ResourceProperties resourceProperties;
+
+	/**
 	 * Resources not to cache.
 	 */
 	@Value(value = "#{'${org.coldis.library.spring.cache.ignore}'.split(',')}")
@@ -23,6 +31,8 @@ public class CacheControlAutoConfiguration implements WebMvcConfigurer {
 	 */
 	@Override
 	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-		registry.addResourceHandler(this.resourcesNotToCache).setCachePeriod(0).setCacheControl(CacheControl.noStore());
+		registry.addResourceHandler(this.resourcesNotToCache)
+		.addResourceLocations(this.resourceProperties.getStaticLocations()).setCachePeriod(0)
+		.setCacheControl(CacheControl.noStore());
 	}
 }
