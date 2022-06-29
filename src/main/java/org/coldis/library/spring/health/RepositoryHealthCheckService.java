e6f9fe6002ba19635182f32e9b0611f715881823
rvcoutinho <me@rvcoutinho.com>
2022-06-29 15:04:35 -0300

    - Calling saving directly;
    Signed-off-by: rvcoutinho <me@rvcoutinho.com>

--- src/main/java/org/coldis/library/spring/health/RepositoryHealthCheckService.java
+++ src/main/java/org/coldis/library/spring/health/RepositoryHealthCheckService.java
@@ -1,6 +1,6 @@
 package org.coldis.library.spring.health;
 
-import org.coldis.library.helper.DateTimeHelper;
+import org.coldis.library.exception.BusinessException;
 import org.coldis.library.model.Typable;
 import org.coldis.library.persistence.keyvalue.KeyValue;
 import org.coldis.library.persistence.keyvalue.KeyValueRepository;
@@ -22,23 +22,24 @@ public class RepositoryHealthCheckService {
 	 * Health check repository.
 	 */
 	@Autowired(required = false)
-	private KeyValueService keyValuService;
+	private KeyValueService keyValueService;
 
 	/**
 	 * Touches the health check repository.
 	 *
-	 * @return The health check value.
+	 * @return                   The health check value.
+	 * @throws BusinessException If the health check does not work.
 	 */
 	@Transactional(
 			propagation = Propagation.REQUIRED,
-			timeout = 3
+			timeout = 1
 	)
-	public HealthCheckValue touch() {
+	public HealthCheckValue touch() throws BusinessException {
 		// Gets the health check object.
-		final KeyValue<Typable> healthCheck = this.keyValuService.lock(HealthCheckService.HEALTH_CHECK_KEY).get();
+		final KeyValue<Typable> healthCheck = this.keyValueService.lock(HealthCheckService.HEALTH_CHECK_KEY).get();
 		// Saves the health check again.
 		healthCheck.setValue(new HealthCheckValue());
-		healthCheck.setUpdatedAt(DateTimeHelper.getCurrentLocalDateTime());
+		this.keyValueService.update(HealthCheckService.HEALTH_CHECK_KEY, healthCheck.getValue());
 		return (HealthCheckValue) healthCheck.getValue();
 	}
 
