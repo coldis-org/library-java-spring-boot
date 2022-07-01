e8896b4bb2a8eefa84af2641198fe217f0ba0a6c
rvcoutinho <me@rvcoutinho.com>
2022-07-01 16:37:51 -0300

    - Formatting;
    Signed-off-by: rvcoutinho <me@rvcoutinho.com>

--- src/main/java/org/coldis/library/spring/jms/JmsConfigurationHelper.java
+++ src/main/java/org/coldis/library/spring/jms/JmsConfigurationHelper.java
@@ -2,13 +2,10 @@ package org.coldis.library.spring.jms;
 
 import javax.jms.ConnectionFactory;
 
-import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.jms.JmsPoolConnectionFactoryFactory;
 import org.springframework.boot.autoconfigure.jms.JmsProperties.AcknowledgeMode;
 import org.springframework.boot.autoconfigure.jms.artemis.ArtemisProperties;
-import org.springframework.boot.autoconfigure.jms.artemis.ExtensibleArtemisConnectionFactoryFactory;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.support.converter.MessageConverter;
@@ -30,8 +27,7 @@ public class JmsConfigurationHelper {
 	public static ConnectionFactory createJmsConnectionFactory(
 			final ListableBeanFactory beanFactory,
 			final ArtemisProperties properties) {
-		return new JmsPoolConnectionFactoryFactory(properties.getPool()).createPooledConnectionFactory(
-				new ExtensibleArtemisConnectionFactoryFactory(beanFactory, properties).createConnectionFactory(ActiveMQConnectionFactory.class));
+		return JmsConfigurationHelper.createJmsConnectionFactory(beanFactory, properties);
 	}
 
 	/**
